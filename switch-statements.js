b63a5ff46f6f834a8676fcc023186a11d0352d85
“rene-weerts” <rjmwrt@gmail.com>
2022-09-30 09:57:29 +0200

    commit-2

---
+++ switch-statements.js
@@ -0,0 +1,15 @@
+const fruit = "Appels";
+
+switch (fruit) {
+    case 'Bananen':
+        console.log('Het zijn bananen!');
+        break;
+    case 'Appels':
+        console.log('Het zijn appels!');
+        break;
+    case 'Citroenen':
+        console.log('Het zijn citroenen!');
+        break;
+    default:
+        console.log('Er is geen vrucht gekozen');
+}
